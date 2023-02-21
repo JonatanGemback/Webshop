84c82c2bda851a65de2f27e5e8a2427eb7797425
JonatanGemback <116281517+JonatanGemback@users.noreply.github.com>
2023-02-21 21:57:32 +0100

    Fixed herrar

--- blandat.js
+++ blandat.js
@@ -1,15 +1,44 @@
+function skapaArtikel(vara) {
+    const artikel = document.createElement("div");
+    artikel.className = 'kläderMönster';
+    const img = document.createElement('img');
+    img.className = 'kläder';
+    img.src = vara.bild;
+    artikel.appendChild(img);
+    const beskrivning = document.createElement('div');
+    beskrivning.className = 'beskrivning';
+    artikel.appendChild(beskrivning);
+    const marke = document.createElement('p');
+    marke.className = 'märke';
+    marke.innerHTML = vara.märke;
+    beskrivning.appendChild(marke);
+    const namn = document.createElement('h4');
+    namn.className = 'kläderNamn';
+    namn.innerHTML = vara.namn;
+    beskrivning.appendChild(namn);
+    const pris = document.createElement('h3');
+    pris.innerHTML = vara.pris;
+    beskrivning.appendChild(pris);
+    return artikel;
+}
+
 function start() {
 
-    var news = document.getElementById("artiklar");
+    var herrar = document.getElementById("herrar");
     var items = lager.artiklar.herrar;
     for (var i = 0; i < items.length; i++) {
-        var h5 = document.createElement("h1");
-        h5.innerHTML = items[i].namn;
-        news.appendChild(h5);
-        var p = document.createElement("p");
-        p.innerHTML = items[i].bild;
-        news.appendChild(p);
+        herrar.appendChild(skapaArtikel(items[i]));
 
     }
 }
 
+/**
+                    <div class="kläderMönster">
+                        <img src="img/brunJacka.jpg" class="kläder">
+                        <div class="beskrivning">
+                            <p class="märke">JACK & JONES</p>
+                            <h4 class="kläderNamn">BRUN JACKA</h4>
+                            <h3>$70</h3>
+                        </div>
+                    </div>
+ */
