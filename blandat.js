0c0ab14323abb152cc699fbd501caa80f6ceb344
JonatanGemback <116281517+JonatanGemback@users.noreply.github.com>
2023-02-23 23:43:54 +0100

    Changed js

--- blandat.js
+++ blandat.js
@@ -1,7 +1,7 @@
 function skapaArtikel(vara) {
     const artikel = document.createElement("a");
     artikel.className = 'kläderMönster';
-    artikel.href = "vara.html?vara=" + vara.namn + "&pris=" + vara.pris;
+    artikel.href = "vara.html?vara=" + vara.namn + "&pris=" + vara.pris + "&bild=" + vara.bild + "&märke=" + vara.märke;
     const img = document.createElement('img');
     img.className = 'kläder';
     img.src = vara.bild;
@@ -10,11 +10,9 @@ function skapaArtikel(vara) {
     beskrivning.className = 'beskrivning';
     artikel.appendChild(beskrivning);
     const marke = document.createElement('p');
-    marke.className = 'märke';
     marke.innerHTML = vara.märke;
     beskrivning.appendChild(marke);
     const namn = document.createElement('h4');
-    namn.className = 'kläderNamn';
     namn.innerHTML = vara.namn;
     beskrivning.appendChild(namn);
     const pris = document.createElement('h3');
@@ -34,3 +32,98 @@ function start() {
     skapaArtiklar(lager.artiklar.damer, document.getElementById("damer"));
     skapaArtiklar(lager.artiklar.barn, document.getElementById("barn"));
 }
+
+function startVara() {
+    const queryString = document.location.search;
+    const params = new URLSearchParams(queryString)
+
+    const bilden = params.get("bild");
+    const märket = params.get("märke");
+    const namnet = params.get("namn");
+    const priset = params.get("pris");
+
+    const artikel = document.createElement("div");
+    artikel.className = "bildSidan";
+    const imgDiv = document.createElement("div");
+    imgDiv.className = "bildPosition";
+    artikel.appendChild(imgDiv);
+    const img = document.createElement("img");
+    img.className = "kläderMönsterVara";
+    img.src = bilden;
+    imgDiv.appendChild(img);
+    const beskrivningSida = document.createElement("div");
+    beskrivningSida.className = "beskrivningSidan";
+    artikel.appendChild(beskrivningSida);
+    const beskrivning = document.createElement("div");
+    beskrivning.className = "beskrivningPosition";
+    beskrivningSida.appendChild(beskrivning);
+    const marke = document.createElement("p");
+    marke.innerHTML = märket;
+    marke.appendChild(beskrivning);
+    const namn = document.createElement("h4");
+    namn.innerHTML = namnet;
+    namn.appendChild(beskrivning);
+    const pris = document.createElement("h3");
+    pris.innerHTML = priset;
+    pris.appendChild(beskrivning);
+    const knapp = document.createElement("select");
+    knapp.appendChild(beskrivning);
+    const storlekS = document.createElement("option");
+    storlekS.appendChild(knapp);
+    const storlekM = document.createElement("option");
+    storlekM.appendChild(knapp);
+    const storlekL = document.createElement("option");
+    storlekL.appendChild(knapp);
+    const storlekXL = document.createElement("option");
+    storlekXL.appendChild(knapp);
+
+    sidorPosition = document.getElementById("sidorPosition");
+    sidorPosition.appendChild(artikel);
+
+}
+/*
+function skapaArtikelVara() {
+    const artikel = document.createElement("div");
+    artikel.className = "bildSidan";
+    const imgDiv = document.createElement("div");
+    imgDiv.className = "bildPosition";
+    artikel.appendChild(imgDiv);
+    const img = document.createElement("img");
+    img.className = "kläderMönsterVara";
+    imgDiv.appendChild(img);
+    const beskrvningSida = document.createElement("div");
+    beskrvningSida.className = "beskrivningSidan";
+    const beskrivning = document.createElement("div");
+    beskrivning.className = "beskrivningPosition";
+    beskrivning.appendChild(beskrvningSida);
+    const marke = document.createElement("p");
+    marke.appendChild(beskrivning);
+    const namn = document.createElement("h4");
+    namn.appendChild(beskrivning);
+    const pris = document.createElement("h3");
+    pris.appendChild(beskrivning);
+    const knapp = document.createElement("select");
+    knapp.appendChild(beskrivning);
+    const storlekS = document.createElement("option");
+    storlekS.appendChild(knapp);
+    const storlekM = document.createElement("option");
+    storlekM.appendChild(knapp);
+    const storlekL = document.createElement("option");
+    storlekL.appendChild(knapp);
+    const storlekXL = document.createElement("option");
+    storlekXL.appendChild(knapp);
+
+}*/
+
+/*      <div id="bildSidan">
+            <div id="bildPosition">
+                <img src="img/brunJacka.jpg" class="kläderMönsterVara">
+            </div>
+        </div>
+        <div id="beskrivningSidan">
+            <div id="beskrivningPosition">
+                <p>Jack & Jones</p>
+                <h4>Brun Jacka</h4>
+                <h3>$70</h3>
+            </div>
+        </div>*/
